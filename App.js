9b6269884d5fc354c8397e155c38e4e31043c9b6
caroline mariotto <carol.mariiotto@gmail.com>
2022-04-13 19:28:29 -0300

    aula

--- App.js
+++ App.js
@@ -9,16 +9,17 @@ import { computeWindowedRenderLimits } from 'react-native/Libraries/Lists/Virtua
 {/* STATE É MUTAVEL E O PROPS É UMA CARACTERISTICA IMUTAVEL*/}
 const App = () => {
 
-  const [fofoca, setFofoca] = useState('nao estou sabendo')
-  const onClickHandler = ()=>{
-    setFofoca('a maria terminou o noivado e fugiu para minas com o kako')
-  }
-
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Você está sabendo da fofoca</Text>
-      <Text style={styles.text}> {fofoca}</Text>
-      <Button title='me conta a fofoca' onPress={onClickHandler}></Button>
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view2}>
+      <Text style={styles.text}>2</Text>
+      </View>
+      <View style={styles.view3}>
+      <Text style={styles.text}>3</Text>
+      </View>
     </View>
   );
 };
@@ -26,16 +27,35 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#0000ff',
-    alignItems: 'center',
+    backgroundColor: '#0ff',
+    alignItems: "stretch",
     justifyContent: 'center',
+    flexDirection: 'column-reverse'
   },
   text: {
     color: '#ffffff',
-    fontSize: 20,
+    fontSize: 50,
     fontStyle: 'italic',
     margin: 10,
   },
+  view1:{
+    flex: 1,
+    backgroundColor: '#00ffff',
+    alignItems: 'center',
+    justifyContent:'center',
+  },
+  view2:{
+    flex: 1,
+    backgroundColor: '#000fff',
+    alignItems: 'center',
+    justifyContent:'center',
+  },
+  view3:{
+    flex: 1,
+    backgroundColor: '#99ff',
+    alignItems: 'center',
+    justifyContent:'center',
+  }
 });
 
 export default App;
