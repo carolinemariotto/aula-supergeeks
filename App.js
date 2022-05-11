55c211794245b6f361dd05df4e27084bc40c6277
caroline mariotto <carol.mariiotto@gmail.com>
2022-05-11 19:22:45 -0300

    comit

--- App.js
+++ App.js
@@ -1,41 +1,61 @@
 import React, {useEffect, useState} from 'react'
-import {SafeAreaView,View,FlatList, Image, Text} from 'react-native'
+import {SafeAreaView,View,FlatList, Image, Text,StyleSheet} from 'react-native'
 
 export default function App(){
-    const [pokemons, setPokemons ]= useState([])
-    useEffect(()=>{
-        fetch('https://pokeapi.co/api/v2/pokemon', {
-            method:'GET',
-            headers:{
-                'Accept':'application/json'
-            } 
+    const [allMovies, setAllMovies] = useState ([])
+    
+    useEffect (()=>{
+        //fetch busca os dados no site
+        //then vai transformar a resposta em um json 
+        // then data vai transforma os dados mapeando o array 
+    })
+
+    fetch('https://ghibliapi.herokuapp.com/films')
+    .then(response=>response.json())
+    .then(data=> {
+        data.forEach(movie=>{
+            console.log(movie.title)
+            console.log(movie.description)
+            console.log(movie.release_date)
+            
         })
-        .then(response=> response.json())
-        .then(date=>{
-            console.log(date)
-            setPokemons(date.results)
-        })
+        setAllMovies(data);
+    }).catch(err=>{
+        console.log(err) //data em vez de movie por causa que arquivo do .then é uma variável chamada data 
+
     },[])
     return(
-        <SafeAreaView>
-            <FlatList
-                data={pokemons}
-                keyExtractor={(pokemon)=> pokemon.name}
-                contentContainerStyle={{flexGrow:1}}
-                renderItem={PokemonShow}
-            />
-        </SafeAreaView>
+        <View style={styles.body}>
+            {/* flatlist exibe uma sequencia de imagens */}
+            <Text>Ghibli Movies</Text>
+        <FlatList 
+            data={allMovies}
+            renderItem={({item})=>
+                <View>
+                    <Text style={styles.text}>{item.title} </Text>
+                    <Image
+                        style={{width: 300, height: 500}}
+                        source={{uri: item.image}}
+                        />
+                </View>
+            }
+        />
+        </View>   
     )
+   
+
 }
-function PokemonShow (item){
-    const {name,url} = item.item
-    const pokemonNumber = url.replace('https://pokeapi.co/api/v2/pokemon','')
-    const ImageUrl = 'https://cdn.traction.one/pokedex/pokemon'+pokemonNumber+'.png'
-    return(
-        <View Style={{flexDirection: 'row'}}>
-            <Image style={{width: 100, height:100}} source={{uri: ImageUrl.replace('/.png','.png')}}/>
-            <Text>{name}</Text>
-        </View>
-    )
-}
+
+const styles = StyleSheet.create({
+    body: {
+        backgroundColor: "#9FFF",
+        alingItens: "center",
+    },
+
+    text: {
+        color: '#fff',
+        fontSize: 28,
+        fontSize:'italic'       
+    }
+});
 
